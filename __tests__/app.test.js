9dd18efaf29e11951ad4ace396876f87aea24b17
Kevin Do <kevind0501@gmail.com>
2022-03-15 15:58:33 -0700

    passing test of hardcode connecting the controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const req = require('express/lib/request');
 
 describe('backend-any-api routes', () => {
   beforeEach(() => {
@@ -10,5 +11,17 @@ describe('backend-any-api routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should create an AoT characters', async () => {
+    const res = await request(app)
+      .post('/api/v1/aot')
+      .send({ name: 'Eren Jaeger', branch: 'Survey Corps' });
+
+    expect(res.body).toEqual({
+      id: expect.any(Number),
+      name: 'Eren Jaeger',
+      branch: 'Survey Corps',
+    });
   });
 });
